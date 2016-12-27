095adc792dd3ed43d5290bfd9fb22ff33bcbd8d3
Stefan Hartmann <stefanhartmann@gmail.com>
2016-12-27 10:04:37 -0500

    add error handling for pet description and media

--- app/controllers/api/v1/pets_controller.rb
+++ app/controllers/api/v1/pets_controller.rb
@@ -1,14 +1,35 @@
 class Api::V1::PetsController < ApplicationController
 
   def index
-    puts "these are the params: #{params}"
+    puts "these are the params: #{params[:location]}"
     puts "this is the current time: #{Time.now()}"
     puts ENV["PF_KEY"]
-    zip = params[:zip]
+    location = params[:location]
     @pets = Pet.all
-    response = HTTParty.get("http://api.petfinder.com/pet.find?location=#{zip}&output=full&format=json&key=#{ENV['PF_KEY']}")
+    response = HTTParty.get("http://api.petfinder.com/pet.find?location=#{location}&output=full&format=json&key=#{ENV['PF_KEY']}")
     @petsFromAPI = [];
+
     response['petfinder']['pets']['pet'].each do |pet|
+
+      puts "pet media contents: #{pet['media']}"
+      if pet['media'].empty?
+        puts "#{pet['name']['$t']}'s photos are empty'"
+        pet['media'] = {"photos" => {"photo" => [{"@size" => 'x',"$t" => 'https://metrouk2.files.wordpress.com/2015/04/animal-comp.png',"@id" => '1'}]}}
+        puts "pet[media]: #{pet['media']}"
+        pet['media']['photos']['photo'].push(
+
+        )
+        puts "#{pet['media']['photos']['photo'][0]}"
+      else
+        pet
+      end
+
+      if pet['description'].empty?
+        pet['description']['$t'] = 'Description unavailable'
+      else
+        pet
+      end
+
       @petsFromAPI.push(
         {
           city: pet['contact']['city']['$t'],
